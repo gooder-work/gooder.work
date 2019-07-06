2baf03736293abfd008200d1f14ec143b789eb24
Phillip Malboeuf <phil@phils.computer>
2019-07-06 00:20:33 -0400

    db as a class attribute

--- server/models/posting.ts
+++ server/models/posting.ts
@@ -1,4 +1,4 @@
-import { FilterQuery, ObjectId } from 'mongodb'
+import { FilterQuery, ObjectId, Db } from 'mongodb'
 
 import { db } from '../clients/mongodb'
 
@@ -9,21 +9,23 @@ export interface PostingDocument {
 
 export default class Posting {
   static collection = 'postings'
+  static db: Db
 
   static async list(filters: FilterQuery<PostingDocument>): Promise<PostingDocument[]> {
-    return (await db()).collection(this.collection).find(filters).toArray()
+    return this.db.collection(this.collection).find(filters).toArray()
   }
 
   static async one(_id: ObjectId) {
-    return (await db()).collection(this.collection).findOne({ _id })
+    return this.db.collection(this.collection).findOne({ _id })
   }
 
   static async create(data: PostingDocument): Promise<PostingDocument> {
-    const collection = (await db()).collection(this.collection)
-    const result = await collection.insertOne({
+    const result = await this.db.collection(this.collection).insertOne({
       created_at: new Date(),
       ...data
     })
     return this.one(result.insertedId)
   }
 }
+
+db().then(db => Posting.db = db)
