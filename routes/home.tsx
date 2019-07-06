2527898437909815702db8bae5db7c738dd8d7df
Phillip Malboeuf <phil@phils.computer>
2019-07-06 10:55:45 -0400

    react-fetching-library

--- routes/home.tsx
+++ routes/home.tsx
@@ -1,40 +1,25 @@
 
 import React from 'react'
-import { Component } from 'react'
+import { FunctionComponent } from 'react'
 import { Link, RouteComponentProps } from 'react-router-dom'
 
+import { Query } from 'react-fetching-library'
 import { PostingDocument } from '../server/models/posting'
 
-import { request } from '../helpers/request'
 
 
-interface Props extends RouteComponentProps<any> {}
-interface State {
-  postings: PostingDocument[]
-}
-
-export class Home extends Component<Props, State> {
-  constructor(props: Props) {
-    super(props)
-    this.state = {
-      postings: []
-    }
-  }
-
-  componentDidMount() {
-    request('get', 'featured_postings').then((postings: PostingDocument[])=> {
-      this.setState({ postings })
-    })
-  }
-
-  public render() {
-    return <>
+export const Home: FunctionComponent<RouteComponentProps<any>> = props => {
+  return <Query<PostingDocument[]> action={{
+    method: 'GET',
+    endpoint: '/featured_postings'
+  }}>
+    {({ loading, error, payload: postings, query }) => <>
       <p><Link to='/search'>Search</Link></p>
       <ul>
-        {this.state.postings.map(posting => <li key={posting._id}>
+        {!loading && postings.map(posting => <li key={posting._id}>
           <Link to={`/postings/${posting._id}`}>{posting.title}</Link>
         </li>)}
       </ul>
-    </>
-  }
+    </>}
+  </Query>
 }
--- routes/search.tsx
+++ routes/search.tsx
@@ -1,15 +1,10 @@
 
 import React from 'react'
-import { Component } from 'react'
+import { FunctionComponent } from 'react'
 import { Link, RouteComponentProps } from 'react-router-dom'
 
 import { Button } from '../components/button'
 
-interface Props extends RouteComponentProps<any> {}
-interface State {}
-
-export class Search extends Component<Props, State> {
-  public render() {
-    return <p>Search here <Button>Search</Button></p>
-  }
+export const Search: FunctionComponent<RouteComponentProps<any>> = props => {
+  return <p>Search here <Button>Search</Button></p>
 }
