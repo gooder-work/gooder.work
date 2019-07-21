e163e68925c61cbdacb3d6141396c84209ac7ab6
Phillip Malboeuf <phil@phils.computer>
2019-07-21 17:07:14 -0400

    email form

--- src/routes/search.tsx
+++ src/routes/search.tsx
@@ -4,7 +4,17 @@ import { FunctionComponent } from 'react'
 import { Link, RouteComponentProps } from 'react-router-dom'
 
 import { Button } from '../components/button'
+import { mutate } from '../components/query'
 
 export const Search: FunctionComponent<RouteComponentProps<any>> = props => {
-  return <p>Search here <Button>Search</Button></p>
+  return <>
+    <p>Search here <Button>Search</Button></p>
+    <form onSubmit={e => {
+      e.preventDefault()
+      mutate('notification_signup', { email: e.currentTarget.email.value }) 
+    }}>
+      <input type='email' name='email' placeholder='Email' />
+      <button type='submit'>Send</button>
+    </form>
+  </>
 }
